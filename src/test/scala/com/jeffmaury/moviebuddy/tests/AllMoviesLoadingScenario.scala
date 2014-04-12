9a4697a22d4ab4bce06d0e1381cecc0d12ea92a2
Stephane Landelle <slandelle@excilys.com>
2014-04-12 11:43:19 +0200

    Upgrade to 2.0.0-SNAPSHOT

--- src/test/scala/com/jeffmaury/moviebuddy/tests/AllMoviesLoadingScenario.scala
+++ src/test/scala/com/jeffmaury/moviebuddy/tests/AllMoviesLoadingScenario.scala
@@ -3,25 +3,22 @@ package com.jeffmaury.moviebuddy.tests
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import io.gatling.jdbc.Predef._
-import io.gatling.http.Headers.Names._
+import io.gatling.http.HeaderNames._
 import io.gatling.http.request._
 import scala.concurrent.duration._
-import bootstrap._
-import assertions._
-
 
 class AllMoviesLoadingScenario extends Simulation {
-  val server = System.getProperty("buddyserver", "http://localhost:8080");
-  val totalUsers = toInt(System.getProperty("gatling.users", "100"));
-  val loops = toInt(System.getProperty("gatling.loops", "1000"));
-  val scn = scenario("Loading all movies (" + totalUsers + " users/" + loops + " loops)").repeat(loops) {
-    exec(
-      http("Loading all movies")
-        .get(server + "/movies")
-        .check(status.is(200)))
-  }
+  val server = System.getProperty("buddyserver", "http://localhost:8080")
+  val totalUsers = Integer.getInteger("gatling.users", 100).toInt
+  val loops = Integer.getInteger("gatling.loops", 1000).toInt
+  val scn = scenario(s"Loading all movies ($totalUsers users/$loops loops)")
+    .repeat(loops) {
+      exec(
+        http("Loading all movies")
+          .get(server + "/movies")
+          .check(status.is(200)))
+    }
 
   setUp(scn
-    .inject(ramp(totalUsers users) over (totalUsers seconds)))
+    .inject(rampUsers(totalUsers) over (totalUsers seconds)))
 }
-
